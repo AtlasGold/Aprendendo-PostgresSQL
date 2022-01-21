fae211dd9c83353bb6a3e2b2a65a9323965557a8
Luan Vinicius Monteiro Dos Santos Silva <72756630+AtlasGold@users.noreply.github.com>
2022-01-21 10:47:25 -0300

    Create lower.sql

---
+++ Tutoriais/Tratamento de Texto/lower.sql
@@ -0,0 +1,11 @@
+-- a função lower deixa todos os caracteres das strings em minusculo
+
+select
+  lower(["coluna com texto"]) -- é interessante deixar as colunas que você quer ultilizar a function dentro de parênteses
+from
+  ["sua tabela"]
+-- essa consulta vai irá converter suas strings de texto escolhidas para minusculo e mostrar na tela
+
+
+
+
