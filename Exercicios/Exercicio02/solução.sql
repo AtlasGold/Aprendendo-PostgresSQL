bf7a9e3fdd89f8a2bcd98a774ed3861012769c97
Luan Vinicius Monteiro Dos Santos Silva <72756630+AtlasGold@users.noreply.github.com>
2022-01-21 10:07:29 -0300

    Solução com Lower

---
+++ Exercicios/Exercicio02/solução.sql
@@ -0,0 +1,6 @@
+SELECT "full_name"
+FROM "[Sua Tabela]"
+WHERE full_name = LOWER(full_name)
+ORDER BY full_name ASC
+
+-- Lower serve para deixar todas as letras em minusculo, você pode saber mais sobre funções de texto na pasta tutoriais.
