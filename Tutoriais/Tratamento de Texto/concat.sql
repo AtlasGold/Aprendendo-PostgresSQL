afb4f05159cd971cf922c48daf6e960c9e4cc60e
Luan Vinicius Monteiro Dos Santos Silva <72756630+AtlasGold@users.noreply.github.com>
2022-01-22 16:35:48 -0300

    Create concat.sql

---
+++ Tutoriais/Tratamento de Texto/concat.sql
@@ -0,0 +1,12 @@
+-- Concat funciona para Concatenar as colunas, mostrar todas elas juntas em uma só coluna 
+
+--Vamos dizer que o nome de um usuario é "Jonas" , seu cpf é 12345678 e sua idade é 20
+
+--Exemplo com CONCAT
+Select CONCAT("Nome","CPF","idade") 
+FROM ["SUA TABELA"]
+GROUP BY "Nome", "CPF" , "idade"   -- sempre que usar uma function dentro de um select devemos agrupar usando um group by as colunas que serão agrupada (Todas)
+
+-- A consulta mostrará 
+-- [Jonas1234567820]
+-- está concatenado ou seja, tudo junto agrupado
